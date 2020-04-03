7de372bb26ca6a1ec3d48c20767794de8ce23fe7
DannyBoy <neo3nt3r@gmail.com>
2020-04-03 12:06:00 -0300

    funcionando em qualquer link

--- webcrawler.py
+++ webcrawler.py
@@ -1,12 +1,15 @@
 from bs4 import BeautifulSoup as bs
 import requests
 
-url = "http://localhost"
+url = input('Digite a url da página: ')
+linkoriginal = url
 while True:
 	page = requests.get(url)
 	soup = bs(page.content, 'html.parser')
 	links = soup.find_all('a')
 	for link in links:
 		url = link.attrs.get('href')
-		url = f"http://localhost/{url}"
+		url = f"{linkoriginal}/{url}"
 		print(url)
+
+		
